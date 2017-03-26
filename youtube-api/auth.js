060b0c05ac9e1b204e8710434ce15bfbf0db39d9
Artem <la-3111@mail.ru>
2017-03-26 17:38:19 +0300

    added youtube api2

--- youtube-api/auth.js
+++ youtube-api/auth.js
@@ -2,7 +2,7 @@
 // at {{ https://cloud.google.com/console }}.
 // If you run this code from a server other than http://localhost,
 // you need to register your own client ID.
-var OAUTH2_CLIENT_ID = '__YOUR_CLIENT_ID__';
+var OAUTH2_CLIENT_ID = 'AIzaSyBUU9hIIs9S0Qo2d7cgoxxoPyh2iEbH5z0';
 var OAUTH2_SCOPES = [
   'https://www.googleapis.com/auth/youtube'
 ];
