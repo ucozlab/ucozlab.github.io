b08cdc70a1f3a2ef3f898d1095b55cfe6348ac29
ucozlab <submerged311@gmail.com>
2020-02-20 22:09:13 +0200

    added solar project

---
+++ solar/precache-manifest.b869fef728b3d7ca5aaf774c96b187c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b74dee6db10d47c0d60a45a41610bda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cb7fb7f7c2c7dbbd964",
+    "url": "/static/js/2.22403f14.chunk.js"
+  },
+  {
+    "revision": "12f058a94e3c710c96388dcf2011a03e",
+    "url": "/static/js/2.22403f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b50bfff6e602f6254f1d",
+    "url": "/static/js/main.7ec84231.chunk.js"
+  },
+  {
+    "revision": "6cf0c71da0e734f92c4d",
+    "url": "/static/js/runtime-main.e0429997.js"
+  }
+]);
